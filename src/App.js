092091e14bdc746544868cc3342212a0b220c59f
Animesh Kumar <animesh.kumar7717@gmail.com>
2023-12-23 11:42:41 +0530

    put the data into action

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React from 'react'
+import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
+import 'react-vertical-timeline-component/style.min.css';
+import timelineElements from './data'
 
 const App = () => {
+
   return (
     <div>
-      App
+      <h1 className='timeline'>Timeline</h1>
+      <VerticalTimeline>
+      {
+        timelineElements.map((item)=>{
+          return (
+            <VerticalTimelineElement className='date' date={item.date} dateClassName={''} key={item.id}>
+
+            </VerticalTimelineElement>
+          )
+        })
+      }
+      </VerticalTimeline>
     </div>
   )
 }
-
+// className='' date={''} dateClassName={''} key={}
 export default App
